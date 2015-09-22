bb6b36119daef9cfb0efed49c973cc2998cf3346
Casey <casey.rauser@quinnipiac.edu>
2015-09-22 18:44:30 +0000

    Add question create view

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+class Question(models.Model):
+  title = models.CharField(max_length=300)
+  description = models.TextField(null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 # Create your models here.
